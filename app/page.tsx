faf33d686bda2f4dd500e5b7639dfb1b8a8999f6
Raffi Hotter <raphael.hotter@gmail.com>
2023-12-06 18:22:59 -0500

    Remove unused css

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { RainbowText } from "./RainbowText";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+    <main className="p-8 md:p-24">
       <div className="max-w-5xl w-full justify-between font-mono text-sm flex flex-col gap-4">
         <Image
           src="/hack.png"
